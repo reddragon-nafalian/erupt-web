d9f94653883b2e1623059a88b0ea11bb95dec48f
陈鹏飞 <1415647852@qq.com>
2024-04-26 11:19:32 +0800

    #### 1.普通标记-点击标记按钮，可以在地图上随意标记，再次点击标记按钮，则不能进行标记。无论什么状态，双击标点则删除对应的标点。重构代码，尽量不操作dom。标点可以储存在数据库中，并且能够正常查看。

--- src/app/build/erupt/components/amap/amap.component.ts
+++ src/app/build/erupt/components/amap/amap.component.ts
@@ -15,6 +15,18 @@ export class AmapComponent implements OnInit {
 
     loading: boolean;
 
+    // 存储标点绘画状态
+    markStatus = false;
+
+    // 存储绑定了this的函数
+    handleMapClickBound: any;
+
+    // 存储所有标记的数组
+    markers: any[] = [];
+
+    // 存储信息窗口
+    infoWindow: any;
+
     @Input() value: any;
 
     @Output() valueChange = new EventEmitter();
@@ -28,13 +40,15 @@ export class AmapComponent implements OnInit {
 
     @ViewChild('tipInput', {
         static: true
-    }) tipInput;
+    }) tipInput: ElementRef;
 
     viewValue: any = "";
 
     loaded: boolean = false;
 
     autocompleteList: any[] = [];
+
+    map: any;
 
     constructor(private lazy: LazyService,
                 private ref: ElementRef,
@@ -43,12 +57,11 @@ export class AmapComponent implements OnInit {
                 private msg: NzMessageService) {
     }
 
-    map: any;
-
-    mouseTool: any;
 
     ngOnInit() {
         this.loading = true;
+        // 绑定函数并重用
+        this.handleMapClickBound = this.handleMapClick.bind(this);
         if (!WindowModel.amapSecurityJsCode) {
             this.msg.error("not config amapSecurityJsCode")
             return;
@@ -60,139 +73,149 @@ export class AmapComponent implements OnInit {
         window["_AMapSecurityConfig"] = {
             securityJsCode: WindowModel.amapSecurityJsCode,
         }
-        this.lazy.loadScript("https://webapi.amap.com/maps?v=2.0&key=" + WindowModel.amapKey).then(() => {
-            if (this.value) {
-                this.value = JSON.parse(this.value);
-                this.autocompleteList = [this.value];
-                this.choiceList(this.value);
-            }
-            this.loading = false;
-            let map = new AMap.Map(this.ref.nativeElement.querySelector("#amap"), {
-                zoom: this.zoom,
-                resizeEnable: true,
-                viewMode: '3D'
-            });
-            map.on("complete", () => {
-                this.loaded = true;
-            });
-            this.map = map;
-            let autoComplete;
-            let placeSearch;
-            AMap.plugin([
-                'AMap.ToolBar',
-                'AMap.Scale',
-                'AMap.HawkEye',
-                'AMap.MapType',
-                'AMap.Geolocation',
-                'AMap.PlaceSearch',
-                'AMap.AutoComplete'
-            ], function () {
-                // 在图面添加工具条控件，工具条控件集成了缩放、平移、定位等功能按钮在内的组合控件
-                map.addControl(new AMap.ToolBar());
-                // 在图面添加比例尺控件，展示地图在当前层级和纬度下的比例尺
-                map.addControl(new AMap.Scale());
-                // 在图面添加鹰眼控件，在地图右下角显示地图的缩略图
-                map.addControl(new AMap.HawkEye({isOpen: true}));
-                // 在图面添加类别切换控件，实现默认图层与卫星图、实施交通图层之间切换的控制
-                map.addControl(new AMap.MapType());
-                // 在图面添加定位控件，用来获取和展示用户主机所在的经纬度位置
-                map.addControl(new AMap.Geolocation({}));
-                autoComplete = new AMap.Autocomplete({
-                    city: ''
-                });
-                placeSearch = new AMap.PlaceSearch({
-                    pageSize: 12, // 单页显示结果条数
-                    children: 0, //不展示子节点数据
-                    pageIndex: 1, //页码
-                    extensions: 'base' //返回基本地址信息
-                });
 
-            });
-            let that = this;
-            this.tipInput.nativeElement.oninput = function () {
-                autoComplete.search(that.tipInput.nativeElement.value, function (status, result) {
-                    if (status == "complete") {
-                        let tips = [];
-                        if (result.tips) {
-                            result.tips.forEach(it => {
-                                if (it.id) {
-                                    tips.push(it);
-                                }
-                            });
-                        }
-                        that.autocompleteList = tips;
-                    } else {
-                        // that.msg.warning(result)
-                    }
-                });
-            };
-
-            document.getElementById("mapOk").onclick = () => {
-                if (!this.value && this.autocompleteList.length > 0) {
-                    this.value = this.autocompleteList[0];
-                    this.viewValue = this.value.name;
-                }
+        this.lazy.loadScript("https://webapi.amap.com/maps?v=2.0&key=" + WindowModel.amapKey)
+            .then(() => {
                 if (this.value) {
-                    if (typeof (this.value) == 'string') {
-                        this.value = JSON.parse(this.value);
-                    }
-                    if (!this.value["id"]) {
-                        this.msg.warning("请选择有效的地址");
-                        return;
-                    }
-                    getDetails(this.value["id"]);
-                } else {
-                    this.msg.warning("请先选择地址");
+                    this.value = JSON.parse(this.value);
+                    this.autocompleteList = [this.value];
+                    this.choiceList(this.value);
                 }
-            };
+                this.loading = false;
+                // 初始化地图
+                let map = new AMap.Map(this.ref.nativeElement.querySelector("#amap"), {
+                    zoom: this.zoom,
+                    resizeEnable: true,
+                    viewMode: '3D'
+                });
+                // 初始化信息窗口
+                this.infoWindow = new AMap.InfoWindow({autoMove: true, offset: new AMap.Pixel(0, -30)});
+                // 地图加载完成回调
+                map.on("complete", () => {
+                    this.loaded = true;
+                });
 
-            if (this.value) {
-                getDetails(this.value.id);
-            }
+                this.map = map;
+                let autoComplete;
+                let placeSearch;
+                AMap.plugin([
+                    'AMap.ToolBar',
+                    'AMap.Scale',
+                    'AMap.HawkEye',
+                    'AMap.MapType',
+                    'AMap.Geolocation',
+                    'AMap.PlaceSearch',
+                    'AMap.AutoComplete',
+                    'AMap.MoveAnimation',
+                    'AMap.Geocoder'
+                ], function () {
+                    // 在图面添加工具条控件，工具条控件集成了缩放、平移、定位等功能按钮在内的组合控件
+                    map.addControl(new AMap.ToolBar());
+                    // 在图面添加比例尺控件，展示地图在当前层级和纬度下的比例尺
+                    map.addControl(new AMap.Scale());
+                    // 在图面添加鹰眼控件，在地图右下角显示地图的缩略图
+                    map.addControl(new AMap.HawkEye({isOpen: true}));
+                    // 在图面添加类别切换控件，实现默认图层与卫星图、实施交通图层之间切换的控制
+                    map.addControl(new AMap.MapType());
+                    // 在图面添加定位控件，用来获取和展示用户主机所在的经纬度位置
+                    map.addControl(new AMap.Geolocation({}));
+                    autoComplete = new AMap.Autocomplete({
+                        city: ''
+                    });
+                    placeSearch = new AMap.PlaceSearch({
+                        pageSize: 12, // 单页显示结果条数
+                        children: 0, //不展示子节点数据
+                        pageIndex: 1, //页码
+                        extensions: 'base' //返回基本地址信息
+                    });
+                });
+                let that = this;
+                this.tipInput.nativeElement.oninput = function () {
+                    autoComplete.search(that.tipInput.nativeElement.value, function (status: string, result: { tips: { id: any; }[]; }) {
+                        if (status == "complete") {
+                            let tips = [];
+                            if (result.tips) {
+                                result.tips.forEach((it: { id: any; }) => {
+                                    if (it.id) {
+                                        tips.push(it);
+                                    }
+                                });
+                            }
+                            that.autocompleteList = tips;
+                        } else {
+                            // that.msg.warning(result)
+                        }
+                    });
+                };
 
-
-            //详情查询
-            function getDetails(id) {
-                placeSearch.getDetails(id, (status, result) => {
-                    if (status === 'complete' && result.info === 'OK') {
-                        placeSearchCallBack(result);
-                        that.valueChange.emit(JSON.stringify(that.value));
-                    } else {
-                        that.msg.warning("找不到该位置信息");
+                document.getElementById("mapOk").onclick = () => {
+                    if (!this.value && this.autocompleteList.length > 0) {
+                        this.value = this.autocompleteList[0];
+                        this.viewValue = this.value.name;
                     }
-                });
-            }
+                    if (this.value) {
+                        if (typeof (this.value) == 'string') {
+                            this.value = JSON.parse(this.value);
+                        }
+                        if (!this.value["id"]) {
+                            this.msg.warning("请选择有效的地址");
+                            return;
+                        }
+                        getDetails(this.value["id"]);
+                    } else {
+                        this.msg.warning("请先选择地址");
+                    }
+                };
 
-            //回调函数
-            function placeSearchCallBack(data) {
-                let poiArr = data.poiList.pois;
-                //添加marker
-                let marker = new AMap.Marker({
-                    map: map,
-                    position: poiArr[0].location
-                });
-                map.setCenter(marker.getPosition());
-                infoWindow.setContent(createContent(poiArr[0]));
-                infoWindow.open(map, marker.getPosition());
-            }
+                if (this.value) {
+                    for (const iter of this.value) {
+                        getDetails(iter.id);
+                    }
+                }
 
-            let infoWindow = new AMap.InfoWindow({
-                autoMove: true,
-                offset: {x: 0, y: -30}
+                //详情查询
+                function getDetails(id: any) {
+                    placeSearch.getDetails(id, (status: string, result: { info: string; }) => {
+                        console.log(result.info);
+                        if (status === 'complete' && result.info === 'OK') {
+                            placeSearchCallBack(result);
+                            that.valueChange.emit(that.value);
+                        } else {
+                            that.msg.warning("找不到该位置信息" + result.info);
+                        }
+                    });
+                }
+
+                //回调函数
+                function placeSearchCallBack(data: { info?: string; poiList?: any; }) {
+                    let poiArr = data.poiList.pois;
+                    //添加marker
+                    for (let i in poiArr){
+                        let marker = new AMap.Marker({
+                            map: map,
+                            position: poiArr[i].location
+                        });
+                        map.setCenter(marker.getPosition());
+                        that.infoWindow.setContent(createContent(poiArr[0]));
+                        that.infoWindow.open(map, marker.getPosition());
+                    }
+                }
+
+                function createContent(poi: { name: string; address: string; tel: string; type: string; location: { lng: string; lat: string; }; }) {  //信息窗体内容
+                    let s = [];
+                    s.push("<b>名称：" + poi.name + "</b>");
+                    s.push("地址：" + poi.address);
+                    s.push("电话：" + poi.tel);
+                    s.push("类型：" + poi.type);
+                    s.push("经度：" + poi.location.lng);
+                    s.push("纬度：" + poi.location.lat);
+                    return s.join("<br>");
+                }
+
             });
-
-            function createContent(poi) {  //信息窗体内容
-                let s = [];
-                s.push("<b>名称：" + poi.name + "</b>");
-                s.push("地址：" + poi.address);
-                s.push("电话：" + poi.tel);
-                s.push("类型：" + poi.type);
-                s.push("经度：" + poi.location.lng);
-                s.push("纬度：" + poi.location.lat);
-                return s.join("<br>");
-            }
-        });
     }
+
+
 
     blur() {
         if (!this.value) {
@@ -220,71 +243,84 @@ export class AmapComponent implements OnInit {
         this.valueChange.emit(null);
     }
 
-    checkType: string;
-
-    overlays: any[];
-
-    draw(type) {
-        //监听draw事件可获取画好的覆盖物
-        this.overlays = [];
-        this.mouseTool.on('draw', (e) => {
-            this.overlays.push(e.obj);
-        });
-        drawFun.call(this, type);
-
-        function drawFun(type) {
-            let fillColor = "#00b0ff";
-            let strokeColor = "#80d8ff";
-            switch (type) {
-                case 'marker': {
-                    this.mouseTool.marker({
-                        //同Marker的Option设置
-                    });
-                    break;
-                }
-                case 'polyline': {
-                    this.mouseTool.polyline({
-                        strokeColor: strokeColor
-                        //同Polyline的Option设置
-                    });
-                    break;
-                }
-                case 'polygon': {
-                    this.mouseTool.polygon({
-                        fillColor: fillColor,
-                        strokeColor: strokeColor
-                        //同Polygon的Option设置
-                    });
-                    break;
-                }
-                case 'rectangle': {
-                    this.mouseTool.rectangle({
-                        fillColor: fillColor,
-                        strokeColor: strokeColor
-                        //同Polygon的Option设置
-                    });
-                    break;
-                }
-                case 'circle': {
-                    this.mouseTool.circle({
-                        fillColor: fillColor,
-                        strokeColor: strokeColor
-                        //同Circle的Option设置
-                    });
-                    break;
-                }
-            }
+    toggleMarker() {
+        this.markStatus = !this.markStatus;
+        if (this.markStatus) {
+            this.map.on('click', this.handleMapClickBound);
+        } else {
+            this.map.off('click', this.handleMapClickBound);
         }
     }
 
-    clearDraw() {
-        this.map.remove(this.overlays);
+    handleMapClick(e) {
+        this.createMarker([e.lnglat.getLng(), e.lnglat.getLat()]);
     }
 
-    closeDraw() {
-        this.mouseTool.close(true); //关闭，并清除覆盖物
-        this.checkType = '';
-    }
+    /**
+     * 创建普通点标记
+     * @param position 经纬度数组，标记在地图上显示的位置
+     * @param title 鼠标滑过点标记时的显示
+     * @param clickable 是否可以点击，默认true
+     * @param draggable 是否可以拖拽，默认false
+     * @param topWhenClick 点击时是否置顶
+     */
+     createMarker(position: any, title?: string, clickable?: boolean, draggable?: boolean, topWhenClick?: boolean) {
+        const marker = new AMap.Marker({
+            map: this.map,
+            title: title,
+            position: position,
+            clickable: clickable,
+            draggable: draggable,
+            topWhenClick: topWhenClick
+        });
+        this.markers.push(marker);
 
+        let geocoder = new AMap.Geocoder({extensions: 'all'});
+
+        // 使用逆地理编码服务获取地址
+        geocoder.getAddress(position, (status, result) => {
+            if (status === 'complete' && result.info === 'OK') {
+                // 获取地址成功
+                let addressComponent = result.regeocode.addressComponent;
+                let pois = result.regeocode.pois;
+                let address = result.regeocode.formattedAddress;
+                // 计算距离最小的poi的typecode
+                let minPoi = pois.reduce((prev, current) => (prev.distance < current.distance) ? prev : current);
+                let typecode = minPoi ? minPoi.type : '未知类型码';
+                let markerInfo = {
+                    name: title || addressComponent.building || addressComponent.neighborhood.name || '未命名位置',
+                    location: position,
+                    id: minPoi.id,
+                    address: address,
+                    district: addressComponent.district,
+                    adcode: addressComponent.adcode,
+                    typecode: typecode,
+                    city: addressComponent.city
+                };
+                // 更新value状态
+                if (Array.isArray(this.value)) {
+                    this.value.push(markerInfo);
+                } else {
+                    this.value = [this.value, markerInfo];
+                }
+                // 触发valueChange事件
+                this.valueChange.emit(JSON.stringify(this.value));
+                // 为每个标记添加双击事件监听器，以便删除标记
+                marker.on('dblclick', () => {
+                    marker.setMap(null);  // 移除标记
+                    this.markers = this.markers.filter(m => m !== marker);  // 从数组中移除对应的标记
+                    if (Array.isArray(this.value)) {
+                        this.value = this.value.filter(v => v.id !== markerInfo.id); // 从value中移除
+                    } else {
+                        this.value = []; // 如果不是数组，直接清空
+                    }
+                    this.valueChange.emit(JSON.stringify(this.value)); // 更新外部组件状态
+                });
+            } else {
+                // 获取地址失败处理逻辑
+                console.error('Geocode is not successful for the following reason: ' + result.info);
+            }
+        });
+    }
 
 }
